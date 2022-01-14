ff4541badbfa0d13b7e5e8cd7570d451118c6d0f
DilvaSof <97153657+DilvaSof@users.noreply.github.com>
2022-01-13 18:22:40 -0600

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ExampleDetailFragment.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
+fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+): View? {
+    // Inflar layout
+    val root = inflater.inflate(R.layout.fragment_example_list, container, false)
+    setupNavigation(root)
+    return root
+}
+
+private fun setupNavigation(root: View) {
+    val detailButton = root.findViewById<Button>(R.id.detail_button)
+    val createButton = root.findViewById<Button>(R.id.create_button)
+    val editButton = root.findViewById<Button>(R.id.edit_button)
+
+    detailButton.setOnClickListener { view: View ->
+        view.findNavController().navigate(R.id.action_List_to_Detail)
+    }
+    createButton.setOnClickListener { view: View ->
+        view.findNavController().navigate(R.id.action_List_to_Add)
+    }
+
+    editButton.setOnClickListener { view: View ->
+        view.findNavController().navigate(R.id.action_Detail_to_Edit)
+}
+}
